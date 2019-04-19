894148bf901b60e459303d39439c080df2882f75
Sébastien <snx.dubois@gmail.com>
2019-04-19 13:53:36 +0200

    easter egg button blanc

---
+++ app/src/main/java/com/example/royaleasterarena/EasterEgg.java
@@ -0,0 +1,13 @@
+package com.example.royaleasterarena;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EasterEgg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.easter_egg);
+    }
+}
